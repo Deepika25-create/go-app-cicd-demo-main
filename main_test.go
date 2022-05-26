b97df8e782ca148ffb09750808caf85eacf45555
Deepika <54930225+Deepika25-create@users.noreply.github.com>
2022-05-26 13:47:37 +0530

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -14,7 +14,7 @@ func TestHomeRoute(t *testing.T) {
 		Home(response, request)
 
 		got := response.Body.String()
-		want := "This is a test for a Deepika-toolchain"
+		want := "This is a test for a toolchain"
 
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
